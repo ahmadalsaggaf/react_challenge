c384c027fb50c677838461d414f2fbac86a430b7
Ahmed Alsaqaf <ahmadalsaggaf30@gmail.com>
2021-08-20 12:11:24 -0400

    define g variables and Implement functions to add data to contacts and appointments

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Switch, Route, Redirect, NavLink } from "react-router-dom";
 
 import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage";
@@ -9,6 +9,10 @@ function App() {
   Define state variables for 
   contacts and appointments 
   */
+ const [contacts, setContacts] = useState([]);
+ const [appointments, setAppointments] = useState([]);
+
+ 
 
   const ROUTES = {
     CONTACTS: "/contacts",
@@ -19,6 +23,14 @@ function App() {
   Implement functions to add data to
   contacts and appointments
   */
+
+  const addContact = (newContact) => {
+    setContacts(prev => [newContact, ...prev])
+  }
+
+  const addAppointment = (newAppointment) => {
+    setAppointments(prev => [newAppointment, ...prev])
+  }
 
   return (
     <>
@@ -37,11 +49,11 @@ function App() {
           </Route>
           <Route path={ROUTES.CONTACTS}>
              {/* Add props to ContactsPage */}
-            <ContactsPage />
+            <ContactsPage contacts={contacts} addContact={addContact} />
           </Route>
           <Route path={ROUTES.APPOINTMENTS}>
             {/* Add props to AppointmentsPage */}
-            <AppointmentsPage />
+            <AppointmentsPage appointments={appointments} addAppointment={addAppointment}/>
           </Route>
         </Switch>
       </main>
